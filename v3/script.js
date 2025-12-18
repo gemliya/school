70c615b445b6f877a66989aaffac9e46370188ea
gemliya <sazaamgrishalobstr@gmail.com>
2025-12-18 11:05:08 +0500

    Add files via upload

---
+++ v3/script.js
@@ -0,0 +1,25 @@
+// const swiper = new Swiper('.swiper', {
+  
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+  
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+    
+//     breakpoints: {
+//       1450: {
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//       },
+//       992: {
+//         slidesPerView: 3,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       }
+//     }
+//   });
+  
